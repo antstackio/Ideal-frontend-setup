b533c33a8e3e1ca663398c805820823387081ad5
ceejeey <muhammed030@gmail.com>
2022-10-24 00:41:55 +0530

    Chores: check

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} alt="" />
+      </div>
+      <div>
+        <img src={reactLogo} alt="" />
+      </div>
+    </>
+  );
+}
+
+export default App;
