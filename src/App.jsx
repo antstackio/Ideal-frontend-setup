bd04ed94d56dc5a7e7e13349fedc2321b20577aa
ceejeey <muhammed030@gmail.com>
2022-10-24 01:32:17 +0530

    Chores: test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log('App ~ count', count);
-
-  return (
-    <>
-      <div>
-        <img src={reactLogo} alt="" />
-      </div>
-      <div>
-        <img src={reactLogo} alt="" />
-      </div>
-    </>
-  );
+  return <Counter />;
 }
 
 export default App;
